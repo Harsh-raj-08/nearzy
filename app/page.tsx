b763481b38a6b2d7c0a380ec632e85506baf62f3
harsh raj <harshraj381017@gmail.com>
2025-05-07 02:14:59 +0530

    added category section for shops and services

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "./components/navbar"
 import Searchbar from "./components/searchbar"
+import FilterCategory from "./components/filterCategory"
 export default function Home() {
   return (
     <>
     <Navbar/>
     <Searchbar/>
+    <FilterCategory/>
     </>
   );
 }
