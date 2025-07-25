b655cc75754c807215f910f4a51f5690c8f9c226
harsh raj <harshraj381017@gmail.com>
2025-07-25 10:23:26 +0530

    cleaned some files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["fakestoreapi.com", "via.placeholder.com"],
-  },
-};
-
-module.exports = nextConfig;
